cc2d54f7b0f3fe0bd65a51f5aea273d89ed1553d
Shuyu <shuyu.zheng@helsinki.fi>
2020-02-25 11:41:52 +0200

    test branch

--- R/detect_ir.R
+++ R/detect_ir.R
@@ -1,4 +1,5 @@
 irDetect <- function(genome){
   genome_rc <- Biostrings::reverseComplement(genome)
   ir <- genome_rc[8000:12000]
+  t <- matchPattern(ir, genome, max.mismatch = 100, with.indels = TRUE)
 }
